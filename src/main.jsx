7d72a47ea672abfbbb8f095fecf9dd20fb5e2a0a
Munem223 <munemshahriar223@gmail.com>
2026-06-27 04:00:38 +0600

    fix router crash

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext";
@@ -8,23 +8,32 @@ import { ThemeProvider } from "./context/ThemeContext";
 import App from "./App";
 import "./styles/index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+]);
+
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: { staleTime: 30_000, retry: 1, refetchOnWindowFocus: false }
+    queries: {
+      staleTime: 30000,
+      retry: 1,
+      refetchOnWindowFocus: false
+    }
   }
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <AuthProvider>
-            <App />
-            <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
-          </AuthProvider>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <AuthProvider>
+          <RouterProvider router={router} />
+          <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
