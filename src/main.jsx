117a830440b2ad340e410449fcd41a5b16ee7779
Munem223 <munemshahriar223@gmail.com>
2026-06-27 04:18:42 +0600

    fix router crash

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "./context/ThemeContext";
 import App from "./App";
 import "./styles/index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  }
-]);
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,13 +20,15 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <AuthProvider>
-          <RouterProvider router={router} />
-          <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
-        </AuthProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <AuthProvider>
+            <App />
+            <Toaster position="top-right" />
+          </AuthProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
